c0a97b84dede8a841a8986c5afbf10868de670e4
AbhayKumar2107 <abhaykumarpatelrbl@gmail.com>
2026-04-16 16:14:10 +0530

    added env file

--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
+import SignUpForm from '../components/SignUpForm';
 import { createUserDocumentFromAuth, signInWithGooglePopup } from '../utils/firebase/firebase'
 
 const SignIn = () => {
 
-    const logGoogleUser =  async()=>{
-        const {user} = await signInWithGooglePopup();
-        
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
 
-  return (
-    <div>
-      i'm  a sign in page
-      <button 
-      onClick={logGoogleUser}
-      className='border-2 p-2'
-      >Sign In with Google Popup</button>
-    </div>
-  )
+    return (
+        <div>
+            i'm  a sign in page
+            <button onClick={logGoogleUser} className='border-2 p-2'>Sign In with Google Popup</button>
+            <SignUpForm />
+        </div>
+    )
 }
 
 export default SignIn
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore, doc, getDoc, setDoc, } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2uxw3EkpmOsEI9E6EAAYRN7uWXGwxDYk",
-  authDomain: "crown-colthing-db.firebaseapp.com",
-  projectId: "crown-colthing-db",
-  storageBucket: "crown-colthing-db.firebasestorage.app",
-  messagingSenderId: "187553095324",
-  appId: "1:187553095324:web:0dc219541abdf7aca15759"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: "crown-colthing-db",
+    storageBucket: "crown-colthing-db.firebasestorage.app",
+    messagingSenderId: "187553095324",
+    appId: "1:187553095324:web:0dc219541abdf7aca15759"
 };
 
 // Initialize Firebase
@@ -19,25 +19,26 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 
 //for authentication
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
-    prompt:"select_account"
+googleProvider.setCustomParameters({
+    prompt: "select_account"
 });
 
-export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const auth = getAuth(firebaseApp);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 
 //for database
-export const db = getFirestore();
+export const db = getFirestore(firebaseApp);
 
-export const createUserDocumentFromAuth = async(userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
 
     const userSnapShot = await getDoc(userDocRef);
 
-    if(!userSnapShot.exists()){
+    if (!userSnapShot.exists()) {
         const { displayName, email } = userAuth;
         const createdAt = new Date();
 
